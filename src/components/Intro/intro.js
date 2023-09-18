a7ac52d57f5bbcfe7dd8938b7a22d93510c15e68
Malvin Opondo <malvinopondo@gmail.com>
2023-09-18 08:00:56 -0700

    feat(Intro.css): Create a file intro.css and import it in App.js

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./intro.css";
+import bg from "../../assets/image.png"
 
 
 function intro() {
   return (
-    <div>intro</div>
+    <section id='intro'>
+        <div className='introContent'>
+
+        </div>
+        <img src={bg} alt='profile' className='bg'/>
+    </section>
   )
 }
 
