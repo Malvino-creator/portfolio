d0979ab3df8043cf2f8a3479b86a6d3b9d60782a
Malvin Opondo <malvinopondo@gmail.com>
2023-09-18 10:16:19 -0700

    feat(Intro.css): Style the hire me button

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 import "./intro.css";
-import bg from "../../assets/image.png"
-
+import bg from "../../assets/image.png";
+import btnImg from "../../assets/hireme.png"
+import {Link} from "react-scroll";
 
 function intro() {
   return (
-    <section id='intro'>
-        <div className='introContent'>
-
-        </div>
-        <img src={bg} alt='profile' className='bg'/>
+    <section id="intro">
+      <div className="introContent">
+        <span className='hello'>Hello, </span>
+        <span className='"intro text'>
+          I'm <span className="introName"> Malvin </span> <br /> Website Designer
+        </span>
+        <p className="introParag"> I am a skilled web designer with experience in creating visually appealling and user friendly websites</p>
+        <Link><button className="btn"><img src={btnImg} alt="hireme" />Hire Me</button></Link>
+      </div>
+      <img src={bg} alt="profile" className="bg" />
     </section>
-  )
+  );
 }
 
-export default intro
+export default intro;
