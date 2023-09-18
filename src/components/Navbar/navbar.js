d11c23ca089aac9473eb811f118600d28e3f2176
Malvin Opondo <malvinopondo@gmail.com>
2023-09-18 06:11:48 -0700

    feat(Navbar): Create a list item in the navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./navbar.css";
 import logo from "../../assets/logo.png";
-import {Link} from "react-scroll";
+import { Link } from "react-scroll";
 
 function navbar() {
   return (
-    <nav className='navbar'>
-        <img src={logo} alt='logo' className='logo'/>
-        <div className='desktopMenu'>
-
-        </div>
-        <button className='desktopMenuBtn'>
-        <img src='' alt='' className='desktopMenuImg'/>
+    <nav className="navbar">
+      <img src={logo} alt="logo" className="logo" />
+      <div className="desktopMenu">
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className="desktopMenuListItem">Clients</Link>
+        <Link className="desktopMenuListItem">About</Link>
+        <Link className="desktopMenuListItem">Portfolio</Link>
+      </div>
+      <button className="desktopMenuBtn">
+        <img src="" alt="" className="desktopMenuImg" />
         Contact Me
-        </button>
+      </button>
     </nav>
-  )
+  );
 }
 
-export default navbar
+export default navbar;
