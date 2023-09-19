0f54668ee771a02a7e593993e5dc5bc98a1d13bc
Malvin Opondo <malvinopondo@gmail.com>
2023-09-19 01:53:25 -0700

    feat(Skills): Import the skill images

--- src/components/Skills/skill.js
+++ src/components/Skills/skill.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "./skill.css";
 import UIDesign from "../../assets/ui-design.png";
 import WebDesign from "../../assets/website-design.png";
@@ -6,22 +6,44 @@ import AppDesign from "../../assets/app-design.png";
 
 function skill() {
   return (
-    <section id='skills'>
-        <span className='skillTitle'> What I do</span>
-        <span className='skillDescription'> I am a skilled and passionate web sesigner with experience in creating visually appealling and user friendly websites.<br />
-         I have a good understanding of design and posses a keen eye for detail. I am also proficient in HTML, CSS, Ruby and JavaScript<br/>
-          as well as Frontend and Backend frameworks such as React and Ruby on Rails.</span>
-          <div className='skillBars'>
-            <div className='skillBar'>
-            <img src='' alt=''></img>
-            <div className='skillBarText'>
-                <h2></h2>
-                <p></p>
-            </div>
-            </div>
+    <section id="skills">
+      <span className="skillTitle"> What I do</span>
+      <span className="skillDescription">
+        {" "}
+        I am a skilled and passionate web sesigner with experience in creating
+        visually appealling and user friendly websites.
+        <br />
+        I have a good understanding of design and posses a keen eye for detail.
+        I am also proficient in HTML, CSS, Ruby and JavaScript
+        <br />
+        as well as Frontend and Backend frameworks such as React and Ruby on
+        Rails.
+      </span>
+      <div className="skillBars">
+        <div className="skillBar">
+          <img src={UIDesign} alt="uidesign"></img>
+          <div className="skillBarText">
+            <h2></h2>
+            <p></p>
           </div>
+        </div>
+        <div className="skillBar">
+          <img src={WebDesign} alt="webdesign"></img>
+          <div className="skillBarText">
+            <h2></h2>
+            <p></p>
+          </div>
+        </div>
+        <div className="skillBar">
+          <img src={AppDesign} alt="appdesign"></img>
+          <div className="skillBarText">
+            <h2></h2>
+            <p></p>
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default skill
+export default skill;
