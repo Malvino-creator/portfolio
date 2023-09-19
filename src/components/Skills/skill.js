2f71066ff1442a70974546c5ccf9baa5350c55f6
Malvin Opondo <malvinopondo@gmail.com>
2023-09-19 01:45:01 -0700

    feat(Skills): Add a skill container for every skill

--- src/components/Skills/skill.js
+++ src/components/Skills/skill.js
@@ -8,7 +8,18 @@ function skill() {
   return (
     <section id='skills'>
         <span className='skillTitle'> What I do</span>
-        <span className='skillDescription'> I am a skilled and passionate web sesigner with experience in creating visually appealling and user friendly websites. I have a good understanding of design and posses a keen eye for detail. I am also proficient in HTML, CSS, Ruby and JavaScript as well as Frontend and Backend frameworks such as React and Ruby on Rails.</span>
+        <span className='skillDescription'> I am a skilled and passionate web sesigner with experience in creating visually appealling and user friendly websites.<br />
+         I have a good understanding of design and posses a keen eye for detail. I am also proficient in HTML, CSS, Ruby and JavaScript<br/>
+          as well as Frontend and Backend frameworks such as React and Ruby on Rails.</span>
+          <div className='skillBars'>
+            <div className='skillBar'>
+            <img src='' alt=''></img>
+            <div className='skillBarText'>
+                <h2></h2>
+                <p></p>
+            </div>
+            </div>
+          </div>
     </section>
   )
 }
