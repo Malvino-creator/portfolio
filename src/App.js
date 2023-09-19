d5d9f84a9393738f1901006e926209a6376ff973
Malvin Opondo <malvinopondo@gmail.com>
2023-09-19 01:38:37 -0700

    feat(Skills): Add a description about my skills

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar/navbar"
 import Intro from "./components/Intro/intro"
+import Skill from "./components/Skills/skill"
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <Skill />
     </div>
   );
 }
--- src/components/Skills/skill.js
+++ src/components/Skills/skill.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import "./skill.css";
+import UIDesign from "../../assets/ui-design.png";
+import WebDesign from "../../assets/website-design.png";
+import AppDesign from "../../assets/app-design.png";
 
 function skill() {
   return (
-    <div>skill</div>
+    <section id='skills'>
+        <span className='skillTitle'> What I do</span>
+        <span className='skillDescription'> I am a skilled and passionate web sesigner with experience in creating visually appealling and user friendly websites. I have a good understanding of design and posses a keen eye for detail. I am also proficient in HTML, CSS, Ruby and JavaScript as well as Frontend and Backend frameworks such as React and Ruby on Rails.</span>
+    </section>
   )
 }
 
