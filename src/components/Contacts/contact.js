f469adae5bdba21bdc4f12720b1c9ffc25f01cab
Malvin Opondo <malvinopondo@gmail.com>
2023-09-21 14:57:30 -0700

    feat(Contact): Import the clients images

--- src/components/Contacts/contact.js
+++ src/components/Contacts/contact.js
@@ -1,5 +1,9 @@
-import React from 'react'
-import "./contact.css"
+import React from 'react';
+import "./contact.css";
+import Walmart from "../../assets/walmart.png";
+import Adobe from "../../assets/adobe.png";
+import Microsoft from "../../assets/microsoft.png";
+import Facebook from "../../assets/facebook.png"
 
 function contact() {
   return (
@@ -9,10 +13,10 @@ function contact() {
         <p className='clientDescription'> I have had the opportunity to work with a diverse group of companies/
         Some of the notable startups I have worked with includes:</p>
         <div className='clientImages'>
-        <img src='clientImage' alt='clientImage' className='clientImage'/>
-        <img src='clientImage' alt='clientImage' className='clientImage'/>
-        <img src='clientImage' alt='clientImage' className='clientImage'/>
-        <img src='clientImage' alt='clientImage' className='clientImage'/>
+        <img src={Walmart} alt='clientImage' className='clientImage'/>
+        <img src={Adobe} alt='clientImage' className='clientImage'/>
+        <img src={Microsoft} alt='clientImage' className='clientImage'/>
+        <img src={Facebook} alt='clientImage' className='clientImage'/>
         </div>
         </div>
         <div id='contact'>
