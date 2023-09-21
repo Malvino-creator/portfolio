616f8fb986f3acb5afcc18f5f4b71413d7734603
Malvin Opondo <malvinopondo@gmail.com>
2023-09-21 06:42:15 -0700

    feat(Works.css): Styling the portfolio section

--- src/components/Works/work.js
+++ src/components/Works/work.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import "./works.css"
 import portfolio1 from "../../assets/portfolio1.png"
+import portfolio2 from "../../assets/portfolio2.png"
+import portfolio3 from "../../assets/portfolio3.png"
+import portfolio4 from "../../assets/portfolio4.png"
+import portfolio5 from "../../assets/portfolio5.png"
+import portfolio6 from "../../assets/portfolio5.png"
 
 function work() {
   return (
@@ -8,12 +13,12 @@ function work() {
         <h2 className='workTitle'>My Portfolio</h2>
         <span className='workDescription'></span>
         <div className='worksImages'>
-            <img src='' alt='' className='workImg'/>
-            <img src='' alt='' className='workImg'/>
-            <img src='' alt='' className='workImg'/>
-            <img src='' alt='' className='workImg'/>
-            <img src='' alt='' className='workImg'/>
-            <img src='' alt='' className='workImg'/>
+            <img src={portfolio1} alt='portfolio1' className='workImg'/>
+            <img src={portfolio2} alt='portfolio2' className='workImg'/>
+            <img src={portfolio3} alt='portfolio3' className='workImg'/>
+            <img src={portfolio4} alt='portfolio4' className='workImg'/>
+            <img src={portfolio5} alt='portfolio5' className='workImg'/>
+            <img src={portfolio6} alt='portfolio6' className='workImg'/>
         </div>
     </section>
   )
