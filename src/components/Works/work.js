3bbd580a1f42da4b5f2335edd28f9329544e35a0
Malvin Opondo <malvinopondo@gmail.com>
2023-09-21 14:23:27 -0700

    feat(Works): Add a more info button

--- src/components/Works/work.js
+++ src/components/Works/work.js
@@ -9,13 +9,15 @@ function work() {
   return (
     <section id='works'>
         <h2 className='workTitle'>My Portfolio</h2>
+        <p className='workParag'> In today's fast-paced digital world, web developer's portfolio serves as a testament to ones skills, <br/>creativity, and ability to craft engaging online experiences.
+         As a React developer,<br/> my portfolio embodies not only the projects I've<br/>undertaken but also the passion and dedication I bring to the ever-evolving field of web development.</p>
         <div className='workImages'>
             <img src={portfolio1} alt='portfolio1' className='workImg'/>
             <img src={portfolio2} alt='portfolio2' className='workImg'/>
             <img src={portfolio3} alt='portfolio3' className='workImg'/>
             <img src={portfolio4} alt='portfolio4' className='workImg'/>
-            
         </div>
+        <button className='worksBtn'> See more </button>
     </section>
   )
 }
